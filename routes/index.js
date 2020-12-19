fc39eefea2431c5dc4d4f4235f427872d269e80e
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2020-12-19 18:21:38 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const { body } = require("express-validator");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  var postdata = req.app.get("postStorrage");
+
+  res.render("index", { title: "WASC", posts: postdata });
+});
+
+router.post("/create", body("*").trim().escape(), function (req, res, next) {
+  var local_message = req.body.content;
+  var local_author = req.body.author;
+  console.log("Sent message: " + local_message);
+  console.log("from: " + local_author);
+
+  req.app.get("postStorrage").push({
+    author: local_author,
+    message: local_message
+  });
+
+  res.redirect("/index");
 });
 
 module.exports = router;
